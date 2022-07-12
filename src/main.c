4caeef0d9d3ee6a269054999b37d9187b80fe606
lucas Workspace <mrllps07@gmail.com>
2022-07-13 00:25:25 +0200

    fix: history visualization

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: llopes-n < llopes-n@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 00:53:02 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/07/12 21:46:14 by llopes-n         ###   ########.fr       */
+/*   Updated: 2022/07/13 00:17:58 by llopes-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 int	main(void)
 {
 	char	*std_in;
+	char	*shell_name;
 
+	shell_name = "luluShell>";
 	while (1)
 	{
-		printf("luluShell>");
-		std_in = readline(NULL);
+		std_in = readline(shell_name);
 		add_history(std_in);
 	}
 	return (0);
