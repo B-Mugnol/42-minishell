3c4d94825d103c3c9d34518bbccd7519c469f8de
B-Mugnol <bruno_mugnol@hotmail.com>
2022-07-22 22:33:58 +0200

    feat: cd

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 22:23:34 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/07/22 04:18:21 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/07/22 21:59:30 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,19 @@ void	ft_unset(t_node **var_lst, t_node **env_lst, char *var_name)
 		return ;
 	lst_delete_var(var_lst, var_name);
 	lst_delete_var(env_lst, var_name);
+}
+
+void	ft_cd(t_node **env_lst, char *path)
+{
+	int		status;
+	char	*pwd;
+
+	status = chdir(path);
+	if (status == -1)
+	{
+		perror(NULL);
+		return ;
+	}
+	pwd = getcwd(NULL, 0);
+	lst_add_var(env_lst, lst_new(ft_strdup("PWD"), pwd));
 }
