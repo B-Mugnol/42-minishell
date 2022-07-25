d77642e401f6b43be748855343dce310431d2a4d
lucas Workspace <mrllps07@gmail.com>
2022-07-25 23:45:59 +0200

    bug: t_var

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: llopes-n < llopes-n@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 00:53:02 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/07/23 03:43:28 by llopes-n         ###   ########.fr       */
+/*   Updated: 2022/07/25 23:24:01 by llopes-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main(void)
 {
-	t_node	*env_lst;
-	t_node	*var_lst;
+	t_var	*env_lst;
+	t_var	*var_lst;
 	char	*std_in;
 	char	*shell_name;
 
@@ -32,7 +32,6 @@ int	main(void)
 			ft_putchar_fd('\n', 1);
 			break ;
 		}
-		hash_search(std_in, env_lst);
 		add_history(std_in);
 		free(std_in);
 	}
@@ -42,7 +41,7 @@ int	main(void)
 	return (0);
 }
 
-void	set_exit_status(t_node **var_lst, int exit_status)
+void	set_exit_status(t_var**var_lst, int exit_status)
 {
 	lst_add_var(var_lst, lst_new(ft_strdup("?"), ft_itoa(exit_status)));
 }
--- src/utils/lst_add.c
+++ src/utils/lst_add.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   lst_add.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: llopes-n < llopes-n@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 01:01:12 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/07/15 22:30:55 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/07/25 23:29:56 by llopes-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lst.h"
 
-t_node	*lst_new(char *name, char *value)
+t_var	*lst_new(char *name, char *value)
 {
-	t_node	*node;
+	t_var	*node;
 
-	node = malloc(sizeof(t_node));
+	node = malloc(sizeof(t_var));
 	node->name = name;
 	node->value = value;
 	node->next = NULL;
 	return (node);
 }
 
-void	lst_add_back(t_node **lst, t_node *new)
+void	lst_add_back(t_var **lst, t_var *new)
 {
-	t_node	*head;
+	t_var	*head;
 
 	if (!*lst)
 		*lst = new;
@@ -39,7 +39,7 @@ void	lst_add_back(t_node **lst, t_node *new)
 	}
 }
 
-void	lst_add_front(t_node **lst, t_node *new)
+void	lst_add_front(t_var **lst, t_var *new)
 {
 	if (!*lst)
 		*lst = new;
@@ -47,9 +47,9 @@ void	lst_add_front(t_node **lst, t_node *new)
 	*lst = new;
 }
 
-void	lst_add_var(t_node **lst, t_node *new)
+void	lst_add_var(t_var **lst, t_var *new)
 {
-	t_node	*old;
+	t_var	*old;
 
 	if (!*lst)
 		*lst = new;
@@ -64,7 +64,7 @@ void	lst_add_var(t_node **lst, t_node *new)
 	lst_add_front(lst, new);
 }
 
-t_node	*find_var(char *name, t_node *lst)
+t_var	*find_var(char *name, t_var *lst)
 {
 	while (lst)
 	{
