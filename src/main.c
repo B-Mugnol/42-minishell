4b1ada831d96c9179fcbb7566b8c66f491bcb09f
B-Mugnol <bruno_mugnol@hotmail.com>
2022-07-27 06:30:16 +0200

    tokenizer tests

--- src/main.c
+++ src/main.c
@@ -3,23 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-n < llopes-n@student.42sp.org.br    +#+  +:+       +#+        */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 00:53:02 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/07/26 20:28:39 by llopes-n         ###   ########.fr       */
+/*   Updated: 2022/07/27 05:47:33 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	print_lst(t_list *tk_lst);
+
 int	main(void)
 {
 	t_var	*env_lst;
 	t_var	*var_lst;
+	t_list	*input_lst;
 	char	*std_in;
 	char	*shell_name;
 
 	shell_name = "luluShell>";
+	env_lst = set_node();
 	var_lst = set_node();
 	set_exit_status(&var_lst, EXIT_SUCCESS);
 	sig_setup();
@@ -32,9 +36,11 @@ int	main(void)
 			break ;
 		}
 		add_history(std_in);
-		tokenizer(std_in);
+		input_lst = tokenizer(std_in);
+		print_lst(input_lst);
 		free(std_in);
 	}
+	ft_lstclear(&input_lst, delete_parse);
 	rl_clear_history();
 	lst_clear(&env_lst);
 	lst_clear(&var_lst);
@@ -44,4 +50,16 @@ int	main(void)
 void	set_exit_status(t_var**var_lst, int exit_status)
 {
 	lst_add_var(var_lst, lst_new(ft_strdup("?"), ft_itoa(exit_status)));
+}
+
+void	print_lst(t_list *tk_lst)
+{
+	while (tk_lst)
+	{
+		if (tk_lst->content != NULL)
+			printf("%s:[%i]\n",
+				((t_parse *)(tk_lst->content))->str,
+				((t_parse *)(tk_lst->content))->type);
+		tk_lst = tk_lst->next;
+	}
 }
