68be2bfaaff0f05fbd540c3d17a24ffdee8dad93
lucas Workspace <mrllps07@gmail.com>
2022-08-19 07:13:03 +0200

    reconize builds

--- src/builtin/builtin.h
+++ src/builtin/builtin.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: llopes-n < llopes-n@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 19:32:51 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/08/12 20:39:38 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/08/19 03:43:57 by llopes-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_H
 # define BUILTIN_H
-
 # include "minishell.h"
+# define BUILDS_COUNT 7
 
 void	ft_cd(t_var **env_lst, char *path);
 void	ft_env(t_var *env_lst);
--- src/hash_builds.c
+++ src/hash_builds.c
@@ -3,36 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   hash_builds.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: llopes-n < llopes-n@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 20:59:12 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/08/09 21:00:11 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/08/19 07:09:44 by llopes-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_bool	hash_search(const char *key, t_var *env)
+t_builtin	*init_builds(void)
 {
-	t_builtin	builds[2];
-	size_t		size;
+	t_builtin	*builds;
+
+	builds = malloc(sizeof(t_builtin) * 7);
+	builds[0] = (t_builtin){"cd", (void *)ft_cd};
+	builds[1] = (t_builtin){"echo"};
+	builds[2] = (t_builtin){"env", (void *)ft_env};
+	builds[3] = (t_builtin){"exit"};
+	builds[4] = (t_builtin){"export", (void *)ft_export};
+	builds[5] = (t_builtin){"pwd", (void *)ft_pwd};
+	builds[6] = (t_builtin){"unset", (void *)ft_unset};
+	return (builds);
+}
+
+char	*hash_search(const char *key, t_builtin *builds)
+{
 	size_t		inx;
 
-	builds[0] = (t_builtin){"pwd", (void *)ft_pwd};
-	builds[1] = (t_builtin){"env", (void *)ft_env};
-	size = sizeof(builds) / sizeof(t_builtin);
 	inx = 0;
-	while (inx < size)
+	while (inx < 7)
 	{
 		if (ft_strncmp(builds[inx].name, key, ft_strlen(builds[inx].name)) == 0)
-		{
-			if (inx == 0)
-				builds[inx].func(NULL);
-			else if (inx == 1)
-				builds[inx].func(env);
-			return (TRUE);
-		}
+			return (builds[inx].name);
 		inx++;
 	}
-	return (FALSE);
+	return (NULL);
+}
+
+char	*search_valid_hash(char *build)
+{
+	char	**builds_lst;
+	int		inx;
+
+	builds_lst = ft_word_split("env pwd echo", ft_isspace);
+	while (builds_lst[inx])
+	{
+		if (ft_strncmp(build, builds_lst[inx], ft_strlen(builds_lst[inx]) == 0))
+			return (build);
+		inx++;
+	}
+	return (NULL);
 }
