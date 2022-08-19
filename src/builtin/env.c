c8e8783a88f4c48f4f832a56e69bdd1624171c19
B-Mugnol <bruno_mugnol@hotmail.com>
2022-08-19 20:49:32 +0200

    now ignores '?' var

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 19:49:39 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/08/12 20:04:30 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/08/19 20:27:41 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@ void	ft_env(t_var *env_lst)
 		return ;
 	while (env_lst)
 	{
-		ft_putstr_fd(env_lst->name, 1);
-		ft_putstr_fd("=", 1);
-		ft_putstr_fd(env_lst->value, 1);
+		if (ft_strncmp(env_lst->name, "?", 2) != 0)
+		{
+			ft_putstr_fd(env_lst->name, 1);
+			ft_putstr_fd("=", 1);
+			ft_putstr_fd(env_lst->value, 1);
+		}
 		env_lst = env_lst->next;
 		if (env_lst)
 			ft_putchar_fd('\n', 1);
