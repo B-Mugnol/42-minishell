152849b8aec1e592b06783f3ca9499895ef0a29f
lucas Workspace <mrllps07@gmail.com>
2022-08-24 23:24:25 +0200

    fix: segf exit_build

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: llopes-n < llopes-n@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 20:34:05 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/08/24 07:25:23 by llopes-n         ###   ########.fr       */
+/*   Updated: 2022/08/24 23:22:52 by llopes-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	exit_build(void)
 	int	exit_status;
 
 	*g_env = var_lst_find_var("?", *(g_env));
+	exit_status = ft_atoi((*g_env)->value);
 	rl_clear_history();
 	var_lst_clear(g_env);
-	exit(ft_atoi((*g_env)->value));
+	exit(exit_status);
 }
