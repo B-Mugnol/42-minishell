61734df396144019817919f9f893e9b5338a94ff
B-Mugnol <bruno_mugnol@hotmail.com>
2022-09-12 18:44:44 +0200

    fix: extra newline when var ? was found

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 19:49:39 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/09/05 18:41:26 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/09/12 18:43:55 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	ft_env(void)
 			ft_putstr_fd(iterator->value, 1);
 		}
 		iterator = iterator->next;
-		if (iterator)
+		if (iterator && ft_strncmp(iterator->name, "?", 2) != 0)
 			ft_putchar_fd('\n', 1);
 	}
 }
--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 22:23:34 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/09/05 20:01:48 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/09/12 18:44:06 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	ft_export(char *usr_in)
 				ft_putstr_fd(iterator->value, 1);
 			}
 			iterator = iterator->next;
-			if (iterator)
+			if (iterator && ft_strncmp(iterator->name, "?", 2) != 0)
 				ft_putchar_fd('\n', 1);
 		}
 	}
