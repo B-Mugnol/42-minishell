56e20079aea33b11b1a373060d4955bd6577d3c0
B-Mugnol <bruno_mugnol@hotmail.com>
2022-09-15 04:10:56 +0200

    fix: moved quote removal to recognize_builds to avoid certain issues ('un"set tem"p' input calling calling builtin when it should fail)

--- src/parsing/build_ins.c
+++ src/parsing/build_ins.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 20:05:56 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/09/14 23:25:07 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/09/15 04:06:45 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int	recognize_builds(char *usr_in, t_builtin *builds)
 {
-	char		**words;
-	int			build_inx;
-	int			inx;
+	char	**words;
+	char	*temp;
+	int		build_inx;
+	int		inx;
 
 	inx = 0;
 	words = ft_word_split(usr_in, ft_isspace);
@@ -24,12 +25,14 @@ int	recognize_builds(char *usr_in, t_builtin *builds)
 		return (7);
 	while (words[inx])
 	{
-		build_inx = hash_search(words[inx], builds);
+		temp = remove_quotes_from_word(words[inx], ft_strlen(words[inx]));
+		build_inx = hash_search(temp, builds);
 		if (build_inx != 7)
 		{
 			ft_free_char_matrix(&words);
 			return (build_inx);
 		}
+		free(temp);
 		inx++;
 	}
 	ft_free_char_matrix(&words);
@@ -47,24 +50,20 @@ void	exec_builds(t_builtin *builds, int build_inx, char *usr_input)
 t_bool	builds(t_type *token_lst, t_shell *st_shell)
 {
 	int			build_inx;
-	char		*tmp;
 	t_builtin	*builds;
 
-	tmp = remove_quotes_from_word(token_lst->str, ft_strlen(token_lst->str));
 	builds = init_builds();
-	build_inx = recognize_builds(tmp, builds);
+	build_inx = recognize_builds(token_lst->str, builds);
 	if (build_inx != 7)
 	{
 		if (valid_hash(build_inx, st_shell->lst_size) == FALSE)
 		{
 			free(builds);
-			free(tmp);
 			return (FALSE);
 		}
 		else
 			exec_builds(builds, build_inx, token_lst->str);
 	}
-	free(tmp);
 	free(builds);
 	return (TRUE);
 }
