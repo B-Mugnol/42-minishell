3d2048b36197acd6d9e8a2bf6cd1f9068393234b
B-Mugnol <bruno_mugnol@hotmail.com>
2022-09-15 03:47:29 +0200

    removed unnecessary function from parsing.c

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-n < llopes-n@student.42sp.org.br    +#+  +:+       +#+        */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 23:34:17 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/09/06 21:06:55 by llopes-n         ###   ########.fr       */
+/*   Updated: 2022/09/15 03:47:02 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,33 +39,4 @@ void	parsing_loop(t_type *token_lst, t_shell *st_shell)
 			token_lst = token_lst->next;
 		}
 	}
-}
-
-char	*find_var_assignment(char *str)
-{
-	char	*start;
-	size_t	inx;
-	size_t	equal_inx;
-
-	inx = 0;
-	while (str[inx])
-	{
-		if (str[inx] == '\"' || str[inx] == '\'')
-			quit_quote(str, &inx);
-		if (str[inx] == '=')
-		{
-			equal_inx = inx;
-			while (inx != 0 && ft_isspace(str[inx]))
-				inx--;
-			if (ft_isspace(str[inx]))
-				inx++;
-			start = str + inx;
-			while (inx != equal_inx)
-				if (!is_valid_varname_char(str[inx++]))
-					return (NULL);
-			return (start);
-		}
-		inx++;
-	}
-	return (NULL);
 }
