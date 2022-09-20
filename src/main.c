61ce6ee92e72984ef681b948d16d32556fee8a18
B-Mugnol <bruno_mugnol@hotmail.com>
2022-09-20 21:06:37 +0200

    feat: program end now returns last known exit code

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-n < llopes-n@student.42sp.org.br    +#+  +:+       +#+        */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 00:53:02 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/09/06 22:28:51 by llopes-n         ###   ########.fr       */
+/*   Updated: 2022/09/20 21:04:24 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 t_var	**g_env;
 
+static int	get_exit_code(void);
+
 int	main(void)
 {
+	int		exit_code;
 	char	*std_in;
 	char	*shell_name;
 	t_shell	st_shell;
@@ -35,6 +38,19 @@ int	main(void)
 		free(std_in);
 	}
 	rl_clear_history();
+	exit_code = get_exit_code();
 	var_lst_clear(g_env);
-	return (0);
+	return (exit_code);
+}
+
+static int	get_exit_code(void)
+{
+	t_var	*current_status;
+	int		exit_code;
+
+	current_status = var_lst_find_var("?", *g_env);
+	if (!current_status)
+		return (EXIT_FAILURE);
+	exit_code = ft_atoi(current_status->value);
+	return (exit_code);
 }
