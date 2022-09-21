4492a069a92d4682bfa0c89da1295e5ee24c53b8
B-Mugnol <bruno_mugnol@hotmail.com>
2022-09-21 20:04:21 +0200

    fix: exit now has no leaks

--- src/builtin/builtin.h
+++ src/builtin/builtin.h
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 19:32:51 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/09/20 22:16:54 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/09/21 19:39:19 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 void	ft_env(void);
 void	ft_pwd(void);
 void	ft_cd(char *usr_in);
-void	ft_exit(char *usr_in);
+void	ft_exit(char *usr_in, t_type *token_lst, t_builtin *builds);
 void	ft_export(char *usr_in);
 void	ft_unset(char *usr_in);
 
