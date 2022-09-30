248419a1273441dbcf7ca13e63ab0132cd3e9c7f
B-Mugnol <bruno_mugnol@hotmail.com>
2022-10-01 01:49:23 +0200

    feat: added check for unclosed quotes

--- src/parsing/tokenizer.c
+++ src/parsing/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 21:48:36 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/09/30 07:07:17 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/10/01 01:42:02 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 #include "parsing.h"
 
 static t_bool	is_empty_str(char *usr_in);
+static t_bool	has_unclosed_quotes(char *str);
 
 t_type	*tokenizer(char *usr_in, t_shell *st_shell)
 {
 	t_type	*lst;
 	size_t	inx;
 
-	if (is_empty_str(usr_in))
+	if (is_empty_str(usr_in) || has_unclosed_quotes(usr_in))
 		return (NULL);
 	lst = NULL;
 	inx = 0;
@@ -55,5 +56,27 @@ static t_bool	is_empty_str(char *usr_in)
 		return (TRUE);
 	}
 	free(aux);
+	return (FALSE);
+}
+
+static t_bool	has_unclosed_quotes(char *str)
+{
+	size_t	simple_quote_count;
+	size_t	double_quote_count;
+	size_t	i;
+
+	simple_quote_count = 0;
+	double_quote_count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '"' && simple_quote_count % 2 == 0)
+			double_quote_count++;
+		if (str[i] == '\'' && double_quote_count % 2 == 0)
+			simple_quote_count++;
+		i++;
+	}
+	if (simple_quote_count % 2 == 1 || double_quote_count % 2 == 1)
+		return (TRUE);
 	return (FALSE);
 }
