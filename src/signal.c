aa0319b499f377ab1c1ed70d587cd33ef321d4ca
B-Mugnol <bruno_mugnol@hotmail.com>
2022-10-02 01:26:27 +0200

    feat: SIGQUIT handling

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 04:00:30 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/09/30 00:16:41 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/10/02 01:12:07 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@ static void	child_signal_handler(int signal);
 
 void	sig_setup(void)
 {
-	struct sigaction	action;
+	struct sigaction	int_action;
+	struct sigaction	quit_action;
 
-	ft_bzero(&action, sizeof(struct sigaction));
-	sigemptyset(&action.sa_mask);
-	action.sa_handler = signal_handler;
-	sigaction(SIGINT, &action, NULL);
+	ft_bzero(&int_action, sizeof(struct sigaction));
+	ft_bzero(&quit_action, sizeof(struct sigaction));
+	sigemptyset(&int_action.sa_mask);
+	sigemptyset(&quit_action.sa_mask);
+	int_action.sa_handler = signal_handler;
+	quit_action.sa_handler = SIG_IGN;
+	sigaction(SIGINT, &int_action, NULL);
+	sigaction(SIGQUIT, &quit_action, NULL);
 }
 
 void	child_sig_setup(void)
@@ -34,6 +39,7 @@ void	child_sig_setup(void)
 	sigemptyset(&action.sa_mask);
 	action.sa_handler = child_signal_handler;
 	sigaction(SIGINT, &action, NULL);
+	sigaction(SIGQUIT, &action, NULL);
 }
 
 static void	signal_handler(int signal)
@@ -49,6 +55,8 @@ static void	signal_handler(int signal)
 
 static void	child_signal_handler(int signal)
 {
+	if (signal == SIGQUIT)
+		return (ft_putendl_fd("Quit (core dumped)", 1));
 	if (signal != SIGINT)
 		return ;
 	set_exit_status(130);
