4dfa09e26268bc38fda03e30b3195e683f05f2d8
B-Mugnol <bruno_mugnol@hotmail.com>
2022-10-03 11:01:48 +0200

    fix: exit status

--- src/errors_frees/error.c
+++ src/errors_frees/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-n <llopes-n@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 22:12:18 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/10/03 05:37:39 by llopes-n         ###   ########.fr       */
+/*   Updated: 2022/10/03 10:59:13 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	error_syntax(char *str)
 		ft_putstr_fd(LULUSHELL_ERROR, 2);
 		ft_putstr_fd(SYNTAX_ERROR, 2);
 		ft_putendl_fd("`newline'", 2);
+		set_exit_status(2);
 		return (FALSE);
 	}
 	if (ft_strchr("<>", str[inx]))
@@ -31,9 +32,8 @@ int	error_syntax(char *str)
 		ft_putstr_fd("`", 2);
 		ft_putchar_fd(str[inx], 2);
 		ft_putendl_fd("'", 2);
-		return (FALSE);
 		set_exit_status(2);
-		inx++;
+		return (FALSE);
 	}
 	return (TRUE);
 }
