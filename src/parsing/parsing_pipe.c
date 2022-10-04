79bc3c7e3049f0fea1e5d3f62f8409bf0c0369eb
B-Mugnol <bruno_mugnol@hotmail.com>
2022-10-05 00:51:11 +0200

    fix: added pipe as last token verification

--- src/parsing/parsing_pipe.c
+++ src/parsing/parsing_pipe.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 21:48:36 by llopes-n          #+#    #+#             */
-/*   Updated: 2022/10/05 00:07:48 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/10/05 00:17:03 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,6 +100,7 @@ static t_bool	has_pipe_syntax_error(char *usr_in)
 		pipe_location = get_unquoted_pipe(words[inx]);
 		if ((pipe_location && pipe_location == words[0])
 			|| (pipe_location && pipe_location[1] == '|')
+			|| (pipe_location && pipe_location[1] == '\0' && !words[inx + 1])
 			|| (pipe_location && pipe_location[1] == '\0'
 				&& get_unquoted_pipe(words[inx + 1])
 				&& get_unquoted_pipe(words[inx + 1]) == words[inx + 1]))
